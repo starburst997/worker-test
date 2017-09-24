db0b31048d2d3d19ccceb8e62a0bc4de243ebeee
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-09-24 15:40:19 -0400

    All tests successfull!

--- worker-test/src/TestOpenFL.hx
+++ worker-test/src/TestOpenFL.hx
@@ -1,4 +1,5 @@
 import openfl.display.Sprite;
+import openfl.events.Event;
 
 import net.rezmason.utils.workers.QuickBoss;
 import net.rezmason.utils.workers.Golem;
@@ -10,10 +11,18 @@ class TestOpenFL extends Sprite
     var boss:TestBoss = null;
     var done:Bool = false;
 
+    var ball:Sprite;
+
     public function new() {
         super();
 
         trace('Hello');
+
+        ball = new Sprite();
+        ball.graphics.beginFill(0x0000FF);
+        ball.graphics.drawCircle(0, 0, 50);
+        ball.graphics.endFill();
+        addChild(ball);
 
         graphics.beginFill(0xFF0000);
         graphics.drawRect(0, 0, 100, 100);
@@ -21,11 +30,25 @@ class TestOpenFL extends Sprite
 
         boss = new TestBoss(Golem.rise('../hxml/TestWorker.hxml'), onDone, null);
         boss.start();
-        boss.send(100);
+
+        #if mobile
+        boss.send(1000000);
+        #else
+        boss.send(10000000);
+        #end
+
+        var xs = 1, ys = 1;
+        addEventListener(Event.ENTER_FRAME, (_) -> {
+            ball.x += xs;
+            ball.y += ys;
+
+            if (ball.x > 200 || ball.x < 0) xs *= -1;
+            if (ball.y > 200 || ball.y < 0) ys *= -1;
+        });
     }
 
     function onDone(results:Array<Int>):Void {
-        trace(results.join('\n'));
+        //trace(results.join('\n'));
 
         graphics.beginFill(0x00FF00);
         graphics.drawRect(0, 0, 100, 100);
